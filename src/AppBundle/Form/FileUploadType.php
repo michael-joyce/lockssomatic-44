6141712c9a865f14b1aca8705b2c0da70d5634b8
Michael Joyce <ubermichael@gmail.com>
2018-01-16 17:40:15 -0800

    cleaning.

--- src/AppBundle/Form/FileUploadType.php
+++ src/AppBundle/Form/FileUploadType.php
@@ -2,11 +2,8 @@
 
 namespace AppBundle\Form;
 
-use AppBundle\Services\FileUploader;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,7 +14,6 @@ class FileUploadType extends AbstractType {
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options) {
-        $maxUpload = min([ini_get('post_max_size'), ini_get('upload_max_filesize')]);
         $builder->add('file', FileType::class, array(
             'label' => $options['label'],
             'required' => true,
