168dbf6a31306f89f0ebf38a9d143b09c858cd64
Michael Joyce <ubermichael@gmail.com>
2018-01-22 11:22:12 -0800

    get phpcs/drupal coding standards going.

--- src/AppBundle/Services/Encoder.php
+++ src/AppBundle/Services/Encoder.php
@@ -1,47 +1,47 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace AppBundle\Services;
 
 /**
- * Description of Encoder
+ * Encode/decode LOCKSS URL-encoded data.
  *
- * @author Michael Joyce <ubermichael@gmail.com>
+ * LOCKSS does a percent-encoding for some things. It's very similar to the
+ * result or rawurlencode(), but the set of characters encoded is slightly
+ * different.
  */
 class Encoder {
-    
+
     /**
-     * LOCKSS does a percent-encoding for some things. It's very similar to the
-     * result or rawurlencode(), but the set of characters encoded is slightly 
-     * different.
-     * 
+     * Encode a string.
+     *
      * @param string $string
+     *   The string to be encoded.
+     *
      * @return string
+     *   The result of the encoding.
      */
     public function encode($string) {
         $callback = function ($matches) {
             $char = ord($matches[0]);
-            return '%'.strtoupper(sprintf('%02x', $char));
+            return '%' . strtoupper(sprintf('%02x', $char));
         };
 
         $encoded = preg_replace_callback('/[^a-zA-Z0-9_* -]/', $callback, $string);
         return str_replace(' ', '+', $encoded);
     }
-    
+
     /**
-     * The encoding is compatible with URL encoding.
-     * 
-     * @param type $string
-     * @return type
+     * Decode a string.
+     *
+     * @param string $string
+     *   The string to decode.
+     *
+     * @return string
+     *   The result of decoding.
      */
     public function decode($string) {
         $decoded = str_replace('+', ' ', $string);
         return rawurldecode($decoded);
     }
-    
+
 }
