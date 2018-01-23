3785fb72d57ff81fc9888e779319a90e1fde6269
Michael Joyce <ubermichael@gmail.com>
2018-01-23 11:05:40 -0800

    cleaning and testing.

--- src/AppBundle/Utilities/Namespaces.php
+++ src/AppBundle/Utilities/Namespaces.php
@@ -17,41 +17,40 @@ use SimpleXMLElement;
  */
 class Namespaces {
 
-    const DCTERMS = 'http://purl.org/dc/terms/';
-    const SWORD = 'http://purl.org/net/sword/';
-    const ATOM = 'http://www.w3.org/2005/Atom';
-    const LOM = 'http://lockssomatic.info/SWORD2';
-    const RDF = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#';
-    const APP = 'http://www.w3.org/2007/app';
-
+    const NS = array(
+        'dcterms' => 'http://purl.org/dc/terms/',
+        'sword' => 'http://purl.org/net/sword/',
+        'atom' => 'http://www.w3.org/2005/Atom',
+        'lom' => 'http://lockssomatic.info/SWORD2',
+        'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+        'app' => 'http://www.w3.org/2007/app',
+    );
+    
     /**
-     * Get the FQDN for the prefix, in a case-insensitive
-     * fashion.
+     * Get the FQDN for the prefix, in a case-insensitive fashion.
      *
      * @param string $prefix
+     *   The usual namespace prefix to dereference.
      *
      * @return string
+     *   The URI or null.
      */
-    public function getNamespace($prefix) {
-        $constant = get_class() . '::' . strtoupper($prefix);
-        if (!defined($constant)) {
-            return;
+    public static function getNamespace($prefix) {
+        if(array_key_exists($prefix, self::NS)) {
+            return self::NS[$prefix];
         }
-
-        return constant($constant);
+        return null;
     }
 
     /**
      * Register all the known namespaces in a SimpleXMLElement.
      *
      * @param SimpleXMLElement $xml
+     *   The document for which namespaces should be registered.
      */
-    public function registerNamespaces(SimpleXMLElement $xml) {
-        $refClass = new ReflectionClass(__CLASS__);
-        $constants = $refClass->getConstants();
-        foreach (array_keys($constants) as $key) {
-            $prefix = strtolower($key);
-            $xml->registerXPathNamespace($prefix, $this->getNamespace($prefix));
+    public static function registerNamespaces(SimpleXMLElement $xml) {
+        foreach (array_keys(self::NS) as $key) {
+            $xml->registerXPathNamespace($key, self::NS[$key]);
         }
     }
 
