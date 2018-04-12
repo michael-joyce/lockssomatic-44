698a267bed6126608eee1f45436fa3c03bdbe0d5
Michael Joyce <ubermichael@gmail.com>
2018-04-12 11:42:09 -0700

    cleaning.

--- src/AppBundle/Command/Lockss/BoxStatusCommand.php
+++ src/AppBundle/Command/Lockss/BoxStatusCommand.php
@@ -11,23 +11,64 @@ namespace AppBundle\Command\Lockss;
 
 use AppBundle\Entity\Box;
 use AppBundle\Entity\BoxStatus;
+use AppBundle\Services\LockssClient;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Description of DaemonStatusCommand
  */
-class BoxStatusCommand extends AbstractLockssCommand {
+class BoxStatusCommand extends ContainerAwareCommand {
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+    
+    /**
+     * @var LockssClient
+     */
+    private $client;
+    
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(EntityManagerInterface $em, LockssClient $client, LoggerInterface $logger) {
+        parent::__construct();
+        $this->client = $client;
+        $this->em = $em;
+        $this->logger = $logger;
+    }
 
     /**
      * Configure the command.
      */
     protected function configure() {
         $this->setName('lockss:box:status');
+        $this->addOption('box', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, "Optional. One or more box IDs to contact.", array());
         $this->setDescription('Report the status of the boxes.');
         parent::configure();
     }
     
+    /**
+     * @return Collection|Box[]
+     */
+    protected function getBoxes($boxIds = array()) {
+        if ($boxIds && count($boxIds)) {
+            return $this->em->getRepository(Box::class)->findBy(array(
+                        'id' => $boxIds
+            ));
+        }
+        return $this->em->getRepository(Box::class)->findAll();
+    }
+
     public function getBoxStatus(Box $box) {
         $status = new BoxStatus();
         $this->em->persist($status);
