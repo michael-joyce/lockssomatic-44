cd50e91abab5f4bf4b5aa1a9358f871af4d3fc71
Michael Joyce <ubermichael@gmail.com>
2018-05-30 10:45:46 -0700

    tiny it of cleaning.

--- src/AppBundle/DataFixtures/ORM/LoadDeposit.php
+++ src/AppBundle/DataFixtures/ORM/LoadDeposit.php
@@ -10,7 +10,6 @@
 namespace AppBundle\DataFixtures\ORM;
 
 use AppBundle\Entity\Deposit;
-use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
